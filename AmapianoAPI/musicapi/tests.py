13122b0ca4e3df9162cae2bf8aabb5fc001db512
Deadownz <mfshowtime@gmail.com>
2024-01-31 07:07:39 -0800

    Update database settings in settings.py

--- AmapianoAPI/musicapi/tests.py
+++ AmapianoAPI/musicapi/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from .models import Artist, Genre, Platform
 
-# Create your tests here.
+class ArtistTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        Artist.objects.create(name='Kabza de Small')
+
+    def test_get_artist_list(self):
+        response = self.client.get('/artists/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Kabza de Small')
+
+class GenreTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        Genre.objects.create(name='Amapiano')
+
+    def test_get_genre_list(self):
+        response = self.client.get('/genres/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Amapiano')
+
+class PlatformTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        Platform.objects.create(name='Spotify')
+
+    def test_get_platform_list(self):
+        response = self.client.get('/platforms/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Spotify')
