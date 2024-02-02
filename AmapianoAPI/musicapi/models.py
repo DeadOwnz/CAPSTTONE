fa69541a70e8d863a6494cf1fb1a7d117bacd758
Deadownz <mfshowtime@gmail.com>
2024-02-02 06:23:03 -0800

    Update to pass test.py

--- AmapianoAPI/musicapi/models.py
+++ AmapianoAPI/musicapi/models.py
@@ -3,12 +3,15 @@
 from django.db import models
 
 class Artist(models.Model):
-    artist_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     genre = models.CharField(max_length=50)
     platform = models.CharField(max_length=50)
 
     def __str__(self):
         return self.artist_name
+
+    class Meta:
+        app_label = 'musicapi'
 
 class Genre(models.Model):
     name = models.CharField(max_length=50)
